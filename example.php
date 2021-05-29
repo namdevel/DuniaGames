7d2f1ef8036c308ef0e88825c9e3e2410bc52dee
几句レし <62225978+namdevel@users.noreply.github.com>
2021-05-30 01:20:01 +0700

    Create example.php

---
+++ example.php
@@ -0,0 +1,9 @@
+<?php
+require('src/duniaGames.php');
+
+$a = new DuniaGames();
+
+echo $a->getUserNameMobileLegends('161307989', '2244');
+echo $a->getUserNameFreeFire('1250078552');
+echo $a->getUserNameCODMobile('243402956362890880');
+echo $a->getUserNameLifeAfter('23235326', 'Obelisk');
